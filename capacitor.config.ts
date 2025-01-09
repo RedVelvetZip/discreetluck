0b5cb44d273414e3e872272fe7b97584dcb033ab
James Holding <james.holding@fynn.io>
2025-01-08 19:05:53 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'discreet-luck',
+  webDir: 'dist'
+};
+
+export default config;
